097db4fd595d24f773d08e4403e24c66ca3911fc
Arsh Rana <q43676709@gmail.com>
2026-09-15 09:46:34 +0530

    15-09

--- lab4/teams.js
+++ lab4/teams.js
@@ -29,7 +29,7 @@ export const getAllTeams = () => teams;
 export const getTeamById = (id) => teams.find((team)=>team.id === id);
 
 export const addTeam = (newTeam) => {
-    const team = { id: nextId++, ...newTeam }
+    const team = {id: nextId++, newTeam}
     teams.push(team)
     return team
 }
@@ -43,7 +43,7 @@ export const updateTeamById = (id, updatedTeam) => {
     return null;
 }
 
-export const deleteTeam = (id) => {
+export const deleteTeamById = (id) => {
     const index = teams.findIndex((team) => team.id === id);
     if(index == -1) return null;
     teams.splice(index, 1)
